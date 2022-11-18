0a3ffe27488cd525de75d0a45b7bc3c319047f1a
ana <105382108+anavoli@users.noreply.github.com>
2022-11-18 19:37:45 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()}  <span role="img" aria-label="love">❤️</span> od <a href="https://nenadjoric.netlify.app">Neška</a></p>
+        <p>&copy; {new Date().getFullYear()}  <span role="img" aria-label="love">❤️</span> od <a href="https://schebet-chat.netlify.app/">Džore</a></p>
       </footer>
     </div>
   )
